7dde5cb0862985fe845724961bcf91411b3bfbca
Dimphy <familiedegoede@live.nl>
2019-03-13 19:31:38 +0100

    users file updated

--- users.php
+++ users.php
@@ -1,23 +1,23 @@
 <?php
 Class User {
-    
+    //attributes
     private $user_id;
     private $voornaam;
     private $achternaam;
     private $email;
-
+    //constructor
     public function __construct($name,$age, $email) {
         $this->name = $name;
         $this->age = $age;
         $this->email = $email;
     }
-
+    //method nieuwe User toevoegen
     public function setUser($voornaam, $achternaam, $email) {
       $this->voornaam = $voornaam;
       $this->achternaam = $achternaam;
       $this->email = $email;
     }
-
+    //method User opvragen
     public function getUser($user_id){
       if(property_exists($this, $user_id)) {
           return $this->$item;
