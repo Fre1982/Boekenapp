733361b522531135cca6936c44842f2cd7791aa3
Laurents Bijlhout <producttesten2017@hotmail.com>
2019-03-13 19:55:13 +0100

    db_connect added

---
+++ db_connect.php
@@ -0,0 +1,38 @@
+<?php
+
+class db
+{
+    protected $host, $username, $password, $dbname;
+    private $conn;
+
+
+    public function __construct($host = "localhost", $username = "root", $password = "", $dbname = 'boekenapp')
+    {
+
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbname = $dbname;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->username, $this->password);
+            // set the PDO error mode to exception
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+    public function getAllData($table = null)
+    {
+        $stmt = $this->conn->prepare("SELECT * FROM " . $table);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $result = $stmt->fetchAll();
+
+
+        var_dump($result);
+
+    }
+}
