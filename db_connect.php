140c7ade2b9218093fc866c885d5e70b076d8f57
Mathias Zabot <mathias.zabot@gmail.com>
2019-03-15 20:33:58 +0100

    changes

--- db_connect.php
+++ db_connect.php
@@ -15,7 +15,7 @@ class db
         $this->dbname = $dbname;
 
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=boekenapp" . $this->dbname, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->username, $this->password);
             // set the PDO error mode to exception
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           //  echo "Connected successfully";
